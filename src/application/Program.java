90cddffc88acb59ce2a4ab633b8ae4babdb3ecf4
Gabriel Xavier <gabrielgx928@gmail.com>
2024-08-30 11:26:49 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
